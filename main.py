f209120e03cabc9c92cc38a95435b3d48d58c18a
Duncan <52967253+TydVirTaal@users.noreply.github.com>
2022-07-05 09:16:21 +0200

    start of main adn word_functions

---
+++ main.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from decouple import config
+
+import word_functions
+
+document_path = Path("")
+template_document = word_functions.WordDoc()
---
+++ word_functions.py
@@ -0,0 +1,6 @@
+import aspose-words as word
+
+
+class WordDoc:
+    def __init__(self, path):
+        self.path = path
