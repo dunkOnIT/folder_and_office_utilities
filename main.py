ee290facab0937fba22d674d125ae82e2a955c85
Duncan <52967253+TydVirTaal@users.noreply.github.com>
2022-07-05 13:05:02 +0200

    working word doc population

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 from pathlib import Path
+import json
 
 from decouple import config
 
 import word_functions
 
-def generate_documents_for_investor(investor_name, list_of_documents)
-# Load in the template
+company_name = config("COMPANY_NAME")
 
-document_path = Path("")
-template_document = word_functions.WordDoc(document_path)
+def generate_documents_for_investor(investor_name: str) -> None:
+    """"""
 
-# 
+    # Targets for replacement values
+    investor_name_target = "[INVESTOR_NAME]"
+    document_name_target = "[DOCUMENT_NAME]"
 
+    # List of documents to generate
+    documents_to_generate=json.loads(config("DOCUMENTS_TO_CREATE"))
+    template_document_path = Path("[TEMPLATE] - Investor Portal Beta Document.docx")
+
+
+    # Loop through documents to generate, and create one of each for the investor
+    for doc_to_generate in documents_to_generate:
+
+        # Create a dict of find value:replace value
+        replacement_mapping = {
+            investor_name_target:investor_name,
+            document_name_target:doc_to_generate
+        }
+
+        # Create the filename for this instance of the document
+        current_filename = f"(BETA) - {company_name} {doc_to_generate} - {investor_name}.docx"
+
+        template_document = word_functions.WordDoc(template_document_path)
+
+        template_document.find_and_replace_in_document(replacement_mapping)
+        template_document.save(current_filename, Path("Investors").joinpath(investor_name))
+
+def find_and_replace_in_document(find_replace_mapping: dict[str, str]):
+    for find_value in find_replace_mapping.keys():
+        pass
+
+
+
+
+def main():
+    """Main function"""
+    generate_documents_for_investor("Duncan Hobbs")
+
+
+if __name__ == "__main__":
+    main()
--- word_functions.py
+++ word_functions.py
@@ -1,6 +1,41 @@
-import aspose.words as word
+from pathlib import Path
+from typing import Union
+
+import aspose.words as aw
+from docx import Document
+from python_docx_replace.docx_replace import docx_replace
+
+doc = aw.Document()
 
 
 class WordDoc:
     def __init__(self, path):
         self.path = path
+        self.document = Document(path)
+
+    def find_and_replace_in_document(self, replacement_mapping: dict[str, str]):
+        """For each key/value pair in the given replacement mapping,
+        iterate through the document and replace each key with its value."""
+
+        for replacement_target in replacement_mapping.keys():
+            print(replacement_target)
+            print(replacement_mapping[replacement_target])
+            # docx_replace(
+            #     self.document,
+            #     replacement_target=replacement_mapping[replacement_target],
+            # )
+            for p in self.document.paragraphs:
+                for run in p.runs:
+                    if replacement_target in run.text:
+                        run.text = run.text.replace(replacement_target, replacement_mapping[replacement_target])
+
+    def save(self, filename: str, path: Union[str, Path, None] = None) -> None:
+        if path is None:
+            save_path = Path(filename)
+        else:
+            if not Path(path).exists():
+                Path(path).mkdir()  # Create the target save directory if it doesn't already exist
+
+            save_path = Path(path).joinpath(filename)
+
+        self.document.save(save_path)
